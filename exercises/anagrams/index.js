14c26725f569ea410403a835ff3b9008a05d7497
Braden Shipley <bradenrshipley@gmail.com>
2019-03-04 14:14:41 -0600

    add capitalize solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,27 +9,39 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 //use regex .replace(/[^\w]/g,'').toLowerCase() to remove punctuation and capital letters
-function buildCharMap(str) {
-	const charMap = {}
-	for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-		charMap[char] = charMap[char] + 1 || 1
-	}
-	return charMap
+// function buildCharMap(str) {
+// 	const charMap = {}
+// 	for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+// 		charMap[char] = charMap[char] + 1 || 1
+// 	}
+// 	return charMap
+// }
+// function anagrams(stringA, stringB) {
+// 	const charMapA = buildCharMap(stringA)
+// 	const charMapB = buildCharMap(stringB)
+// 	//if the size of the objects is different, return false
+// 	if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+// 		return false
+// 	}
+// 	//iterate through one object and see if the values are equal in the other object. return true if no failures show up
+// 	for (let char in charMapA) {
+// 		if (charMapA[char] !== charMapB[char]) {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
+// helper function
+function cleanString(str) {
+	return str
+		.replace(/[^/w]/g, '')
+		.toLowerCase()
+		.split('')
+		.sort()
+		.join('')
 }
-function anagrams(stringA, stringB) {
-	const charMapA = buildCharMap(stringA)
-	const charMapB = buildCharMap(stringB)
-	//if the size of the objects is different, return false
-	if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
-		return false
-  }
-  //iterate through one object and see if the values are equal in the other object. return true if no failures show up
-	for (let char in charMapA) {
-		if (charMapA[char] !== charMapB[char]) {
-			return false
-		}
-	}
-	return true
+//alternative solution
+function anagrams(strA, strB) {
+	return cleanString(strA) === cleanString(strB)
 }
-
 module.exports = anagrams
--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -6,7 +6,21 @@
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
+//make an empty arr 'words
+//split input str by spaces
+//for each word in arr
+//uppercase the first letter of the word
+//join the first letter with the rest of the word
+//push result into words array
+//join words arr into a str
+//return result
+function capitalize(str) {
+	const words = []
+	let arr = str.split(' ')
+	for (let word of arr) {
+		words.push(word[0].toUpperCase() + word.slice(1))
+	}
+	return words.join(' ')
+}
 
-function capitalize(str) {}
-
-module.exports = capitalize;
+module.exports = capitalize
