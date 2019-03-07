ffb141cedb449bf671953030aaa581664c309184
Braden Shipley <bradenrshipley@gmail.com>
2019-03-07 11:29:40 -0600

    add pyramid solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -13,7 +13,48 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//recursive solution
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+	if (row === n) {
+		return
+	}
+	if (level.length == 2 * n - 1) {
+		console.log(level)
+		return pyramid(n, row + 1)
+	}
+	const center = Math.floor((2 * n - 1) / 2)
+	let add
+	if (center - row <= level.length && center + >= level.legnth) {
+		add += '#'
+	} else {
+		add += ' '
+	}
+	pyramid(n, row, level + add)
+}
+// //from - to n (iterate through rows)
+//   //create an empty string, 'level'
+//   //for 0 to ??? (columns)
+//     //if the column should have a #
+//       //add a # to 'level'
+//     //else
+//       //add a space to 'level
+//   //console log 'level'
+//   function pyramid(n) {
+//     //n is the total number of rows
+//     const width = n+n-1
+//     const center = Math.floor(width/2)
+//     for (let row = 0; row < n; row++){
+//       let level = ''
+//       for (let col = 0; col < width; col++){
+//         if (center - row <= col && center + row >= col) {
+//           level+='#'
+//         } else {
+//           level+=' '
+//         }
+//       }
+//       console.log(level)
+//     }
+//   }
 
 module.exports = pyramid;
