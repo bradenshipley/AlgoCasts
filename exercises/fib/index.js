19753be5fba7685b254cd6fa18a2d4b52bc9dcb4
Braden Shipley <bradenrshipley@gmail.com>
2019-03-24 16:17:04 -0500

    Update index.js

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -7,14 +7,27 @@
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
-//recursive solution
+//recursive solution w/out memoization
 function fib(n) {
   if (n < 2) {
     return n
   }
-  return fib(n-1) + fib(n-2)
+  return fib(n - 1) + fib(n - 2)
 }
+//memoize version of fib
+function memoize(fn) {
+  const cache = {}
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args]
+    }
+    const result = fn.apply(this, args)
 
+    cache[args] = result
+    return result
+  }
+}
+ fib = memoize(fib)
 //iterative solution
 // function fib(n) {
 //   let arr = [0,1]
