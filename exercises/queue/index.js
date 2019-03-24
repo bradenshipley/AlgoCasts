dbd3b566c90786814e3ce18dc3ac42d564c7e6db
Braden Shipley <bradenrshipley@gmail.com>
2019-03-24 16:45:34 -0500

    a

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,23 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.data = []
+  }
+  add(x) {
+    this.data.unshift(x)
+    return this.data[0]
+  }
+  remove(x) {
+    return this.data.pop()
+  }
+  length() {
+    return this.data.length
+  }
+  empty() {
+    return this.data.length == 0
+  }
+}
 
 module.exports = Queue;
